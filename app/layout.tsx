263ed619a84bba068ab33632e9e9c5bc75da3a9b
thewoowon <thewoowon@naver.com>
2024-08-11 00:31:49 +0900

    feat: provider, layout, main page 작업

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Providers from "./provider";
+import type { Metadata } from 'next';
+import './globals.css';
+import Providers from './provider';
 
 export const metadata: Metadata = {
-  title: "Junction Asia 2024",
-  description: "Junction Asia 2024",
+  title: '민원이지',
+  description: '빠르고 확실한 민원은 민원이지',
 };
 
 export default function RootLayout({
@@ -14,6 +14,86 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <script
+          type="text/javascript"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&libraries=services,clusterer`}
+          async
+        />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,238 @@
+'use client';
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
-  return <main></main>;
+  const router = useRouter();
+  return (
+    <Main>
+      <svg
+        style={{
+          position: 'absolute',
+          top: '-30%',
+          left: '-40%',
+        }}
+        width="1020"
+        height="1020"
+        viewBox="0 0 1020 1020"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_d_113_1520)">
+          <circle cx="510" cy="506" r="480" fill="white" />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_113_1520"
+            x="0"
+            y="0"
+            width="1020"
+            height="1020"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feMorphology
+              radius="10"
+              operator="dilate"
+              in="SourceAlpha"
+              result="effect1_dropShadow_113_1520"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="10" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0.294746 0 0 0 0 0.529915 0 0 0 0.4 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_113_1520"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_113_1520"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '6px',
+          fontSize: '18px',
+          lineHeight: '18px',
+          zIndex: 10,
+        }}
+      >
+        빠르고 확실한 민원은
+        <div
+          style={{
+            width: '223px',
+            height: '59px',
+            position: 'relative',
+          }}
+        >
+          <Image
+            src={'/images/mwez_text.png'}
+            alt="민원이지"
+            fill
+            sizes="223px"
+          />
+          <div
+            style={{
+              width: '104px',
+              height: '136px',
+              position: 'absolute',
+              right: '-52px',
+              top: '-36px',
+            }}
+          >
+            <Image
+              src={'/images/mwez_deco.png'}
+              alt="민원이지"
+              fill
+              sizes="104px"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          gap: '10px',
+          fontSize: '24px',
+        }}
+      >
+        <div
+          onClick={() => {
+            router.push('/complaint');
+          }}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            cursor: 'pointer',
+            width: '246px',
+            color: '#fff',
+          }}
+        >
+          내 민원 확인하기
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask
+              id="mask0_67_282"
+              style={{
+                maskType: 'alpha',
+              }}
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="24"
+              height="24"
+            >
+              <rect width="24" height="24" fill="#ffffff" />
+            </mask>
+            <g mask="url(#mask0_67_282)">
+              <path
+                d="M8.025 22L6.25 20.225L14.475 12L6.25 3.775L8.025 2L18.025 12L8.025 22Z"
+                fill="#ffffff"
+              />
+            </g>
+          </svg>
+        </div>
+        <div
+          onClick={() => {
+            router.push('/submission');
+          }}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            cursor: 'pointer',
+            width: '246px',
+            color: '#fff',
+          }}
+        >
+          민원 건의하기
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask
+              id="mask0_67_282"
+              style={{
+                maskType: 'alpha',
+              }}
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="24"
+              height="24"
+            >
+              <rect width="24" height="24" fill="#ffffff" />
+            </mask>
+            <g mask="url(#mask0_67_282)">
+              <path
+                d="M8.025 22L6.25 20.225L14.475 12L6.25 3.775L8.025 2L18.025 12L8.025 22Z"
+                fill="#ffffff"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </Main>
+  );
 }
+
+const Main = styled.main`
+  max-width: 480px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  background-color: #005ea9;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding: 164px 32px 110px 32px;
+  position: relative;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 50px;
+  margin-top: 10px;
+  border-radius: 10px;
+  background-color: #000;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+`;
