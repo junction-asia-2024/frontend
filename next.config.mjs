3103b4fb9e00c8ffc77ce14a52620c4725e867ff
thewoowon <thewoowon@naver.com>
2024-08-11 10:31:07 +0900

    feat: image domain 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    loader: 'custom', // 커스텀 로더 사용
+    unoptimized: true, // 이미지 최적화 비활성화
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/6qzLODAqs2g1LZbVYqtuQw/**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+        port: '',
+        pathname: '/**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd1m84t8yekat2i.cloudfront.net',
+        port: '',
+        pathname: '/**/*',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
